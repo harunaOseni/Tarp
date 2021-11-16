ba3ed5d13031a7e85441c0dc42fe5df3f202dc9a
Haruna Oseni <harunaoseni23@gmail.com>
2021-11-16 07:14:20 -0600

    created image folder and added logo image

---
+++ artist_login_page.py
@@ -0,0 +1,29 @@
+from tkinter import * 
+from tkinter import ttk
+
+
+artist_login_page = Tk()
+artist_login_page.title("Tarp (Make Beautiful Art)")
+
+artist_login_page.geometry("700x700")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+artist_login_page.mainloop()
