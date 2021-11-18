2ef60dc7a56586d2d2ec1a0f4c503c9ad1b3653e
Haruna Oseni <harunaoseni23@gmail.com>
2021-11-18 11:27:47 -0600

    beginning interface on main_page.py

--- main_page.py
+++ main_page.py
@@ -10,7 +10,15 @@ def main():
     main_page = Tk()
     main_page.title("Tarp (Make Beautiful Art)")
     main_page.geometry("700x700")
+
+
+    #app icon
+    img = ImageTk.PhotoImage(Image.open("images/logo.jpg"))
+    main_page.iconphoto(False, img)
     
+    #top frame
+    top_frame = Frame(main_page, bg="white")
+    top_frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor="n")
 
 
 
